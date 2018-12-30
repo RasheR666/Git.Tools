f96ba4dfbe37dcab42e6822ee622d4ea01d0a734
RasheR666 <rasher666@gmail.com>
2018-12-31 02:48:49 +0500

    Git.Tools solution with Excel.To.Text.Converter

---
+++ Excel.To.Text.Converter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel.To.Text.Converter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
